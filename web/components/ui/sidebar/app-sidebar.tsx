b6fcc3dfbbe2583aab39f5c5ab102252f5df75f0
REVERSS <reverssecole@gmail.com>
2024-11-05 10:02:32 +0100

    Feat: sidebar done with section and link

---
+++ web/components/ui/sidebar/app-sidebar.tsx
@@ -0,0 +1,86 @@
+"use client"
+
+import * as React from "react";
+import { ChartNoAxesCombined, CreditCard, Grid3x3, Newspaper, PackageSearch, PanelsTopLeft, UsersRound } from "lucide-react";
+import { NavMain } from "@/components/ui/sidebar/nav-main";
+import { NavUser } from "@/components/ui/sidebar/nav-user";
+import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarRail } from "@/components/shadcn-ui/sidebar";
+
+// This is sample data.
+const data = {
+  user: {
+    name: "Jesko",
+    email: "developer@miammiam.com",
+    avatar: "https://ui.shadcn.com/avatars/02.png",
+  },
+
+  dashboard_nav: [
+    {
+      title: "Overview",
+      url: "/dashboard/overview",
+      icon: PanelsTopLeft,
+      isActive: true,
+    },
+    {
+      title: "Statistics",
+      url: "/dashboard/statistics",
+      icon: ChartNoAxesCombined,
+    },
+  ],
+
+  catalog_nav: [
+    {
+      title: "Products",
+      url: "/dashboard/catalog/products",
+      icon: Grid3x3,
+      isActive: true,
+    },
+    {
+      title: "Inventory",
+      url: "/dashboard/catalog/Inventory",
+      icon: PackageSearch,
+    }
+  ],
+
+  customers_nav: [
+    {
+      title: "Users",
+      url: "/dashboard/customers/users",
+      icon: UsersRound,
+    }
+  ],
+
+  sales_nav: [
+    {
+      title: "Orders",
+      url: "/dashboard/sales/orders",
+      icon: CreditCard,
+      isActive: true,
+    },
+    {
+      title: "Invoices",
+      url: "/dashboard/sales/invoices",
+      icon: Newspaper,
+    }
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader/>
+
+      <SidebarContent>
+        <NavMain titleNav={"Dashboard"} items={data.dashboard_nav} />
+        <NavMain titleNav={"Catalog"} items={data.catalog_nav} />
+        <NavMain titleNav={"Customers"} items={data.customers_nav} />
+        <NavMain titleNav={"Sales"} items={data.sales_nav} />
+      </SidebarContent>
+
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
---
+++ web/components/ui/sidebar/nav-main.tsx
@@ -0,0 +1,69 @@
+"use client"
+
+import { type LucideIcon } from "lucide-react"
+
+import {
+  Collapsible,
+} from "@/components/shadcn-ui/collapsible"
+import {
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+} from "@/components/shadcn-ui/sidebar"
+
+export function NavMain({
+  titleNav,
+  items,
+}: {
+  titleNav: string
+  items: {
+    title: string
+    url: string
+    icon?: LucideIcon
+    isActive?: boolean
+    items?: {
+      title: string
+      url: string
+    }[]
+  }[]
+}) {
+  return (
+    <SidebarGroup>
+      <SidebarGroupLabel>{titleNav}</SidebarGroupLabel>
+      <SidebarMenu>
+        {items.map((item) => (
+          <Collapsible
+            key={item.title}
+            asChild
+            defaultOpen={item.isActive}
+            className="group/collapsible"
+          >
+            <SidebarMenuItem>
+              <SidebarMenuButton tooltip={item.title}>
+                {item.icon && <item.icon />}
+                <span>{item.title}</span>
+              </SidebarMenuButton>
+
+              <SidebarMenuSub>
+                {item.items?.map((subItem) => (
+                  <SidebarMenuSubItem key={subItem.title}>
+                    <SidebarMenuSubButton asChild>
+                      <a href={subItem.url}>
+                        <span>{subItem.title}</span>
+                      </a>
+                    </SidebarMenuSubButton>
+                  </SidebarMenuSubItem>
+                ))}
+              </SidebarMenuSub>
+            </SidebarMenuItem>
+          </Collapsible>
+        ))}
+      </SidebarMenu>
+    </SidebarGroup>
+  )
+}
