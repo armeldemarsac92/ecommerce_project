1434c325f3e6bfa47fa64dae3c8c349ae5172ad8
Squekk <105415166+Squekk@users.noreply.github.com>
2024-11-09 13:44:26 +0100

    Fix: Fixing the nullable in Brand api

--- api/Tdev702.Contracts/SQL/Request/Shop/Brand/UpdateBrandRequest.cs
+++ api/Tdev702.Contracts/SQL/Request/Shop/Brand/UpdateBrandRequest.cs
@@ -3,6 +3,6 @@ namespace Tdev702.Contracts.SQL.Request.Shop.Brand;
 public class UpdateBrandRequest
 {
     public required long Id { get; set; }
-    public string Title { get; set; }
-    public string Desription { get; set; }
+    public string? Title { get; set; }
+    public string? Desription { get; set; }
 }
