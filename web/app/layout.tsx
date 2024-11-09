135d541503c3288c79c030137afb0b541d206e33
REVERSS <reverss@REVERSSs-MacBook-Pro.local>
2024-11-09 22:50:28 +0100

    feat: setup shadcn-toast

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -7,6 +7,7 @@ import { Providers } from "./providers";
 import { siteConfig } from "@/config/site";
 import { supreme } from "@/config/fonts";
 import * as React from "react";
+import {Toaster} from "@/components/shadcn/toaster";
 
 export const metadata: Metadata = {
   title: {
@@ -42,23 +43,7 @@ export default function RootLayout({
       >
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
           {children}
-          {/*<div className="relative flex flex-col h-screen">
-            <Navbar />
-            <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
-              {children}
-            </main>
-            <footer className="w-full flex items-center justify-center py-3">
-              <Link
-                isExternal
-                className="flex items-center gap-1 text-current"
-                href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
-                title="nextui.org homepage"
-              >
-                <span className="text-default-600">Powered by</span>
-                <p className="text-primary">NextUI</p>
-              </Link>
-            </footer>
-          </div>*/}
+          <Toaster />
         </Providers>
       </body>
     </html>
