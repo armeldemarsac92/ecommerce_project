3e1ca18d9773fbf36ba75e0611245d042f37cd3c
Armel De Marsac <armeldemarsac@gmail.com>
2025-02-03 10:37:31 +0100

    fix : mappin order request

--- api/Tdev702.Contracts/Mapping/OrderMapping.cs
+++ api/Tdev702.Contracts/Mapping/OrderMapping.cs
@@ -67,7 +67,6 @@ public static class OrderMapping
             UserId = createOrderRequest.UserId,
             TotalAmount = totalAmount,
             
-            PaymentStatus = createOrderRequest.PaymentStatus,
             StripeInvoiceId = createOrderRequest.StripeInvoiceId
         };
     }
