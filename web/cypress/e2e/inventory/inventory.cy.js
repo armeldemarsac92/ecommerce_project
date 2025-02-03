bc462d2e1ffa75622ce5bc2a9f48b6e014c8d9aa
jonathandumesnil <jonathan.dumesnil@epitech.eu>
2025-02-03 10:01:48 +0100

    feat: remove header

--- web/cypress/e2e/inventory/inventory.cy.js
+++ web/cypress/e2e/inventory/inventory.cy.js
@@ -1,7 +1,7 @@
 describe.skip('Inventory page test', () => {
     it('User can see the list of inventory', () => {
         cy.intercept('GET', '**/api/v1/products').as('getInventory');
-
+ 
         cy.visit('/dashboard')
         cy.contains('MiamMiam').should('be.visible')
         cy.visit('/dashboard/catalog/inventory')
