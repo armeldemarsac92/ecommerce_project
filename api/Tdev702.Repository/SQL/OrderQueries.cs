2e70b1359f8c88944bcebebcda3a61a3863e7ec6
Armel De Marsac <armeldemarsac@gmail.com>
2025-03-18 16:39:00 +0100

    fix : fixed the order queries

--- api/Tdev702.Repository/SQL/OrderQueries.cs
+++ api/Tdev702.Repository/SQL/OrderQueries.cs
@@ -31,8 +31,8 @@ public static class OrderQueries
    FROM backoffice.vw_orders_summary
    WHERE user_id = @UserId        
    ORDER BY 
-        CASE WHEN @orderBy = 'DESC' THEN id END DESC,
-        CASE WHEN @orderBy = 'ASC' THEN id END ASC
+        CASE WHEN @orderBy = 2 THEN id END DESC,
+        CASE WHEN @orderBy = 1 THEN id END ASC
     LIMIT @pageSize 
     OFFSET @offset;";
 
